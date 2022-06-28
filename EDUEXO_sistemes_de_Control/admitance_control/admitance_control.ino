9860662a314355ab22be83b7bf8e81285677b501
miknammy98 <36418509+miknammy98@users.noreply.github.com>
2022-06-28 11:20:49 +0200

    Update admitance_control.ino

--- EDUEXO_sistemes_de_Control/admitance_control/admitance_control.ino
+++ EDUEXO_sistemes_de_Control/admitance_control/admitance_control.ino
@@ -1,21 +1,21 @@
-#include <Servo.h>
+#include <Servo.h>                                               // Afegir la llibreria Servo
 
- Servo myservo;
- int forceAnalogInPin = A3;
- const int forceOffset = 439;
- int forceIs;
- int forceDesired = 0;
- int positionDesired = 40;
- float gain = 0.2;
+ Servo myservo;                                                  // Inicialitza el Servomotor
+ int forceAnalogInPin = A3;                                      // Inicialitza els pins del servo
+ const int forceOffset = 439;                                    // Inicialitza un offset per a la galga
+ int forceIs;                                                    // S'inicialitzen les variables de la força (quina és actualment i quina és la força desitjada
+ int forceDesired = 0;                                   
+ int positionDesired = 40;                                       // Posició desitjada
+ float gain = 0.2;                                               // guany del sistema
 
  void setup(){
- myservo.attach(3);
- delay(1000);
+ myservo.attach(3);                                              // Definició del pin del Servomotor
+ delay(1000);                                                    // Retard d'1 segon
  }
 
  void loop(){
- forceIs = analogRead(forceAnalogInPin)-forceOffset;
- delay(10);
- positionDesired -= gain*(forceIs-forceDesired);
- myservo.write(positionDesired);
+ forceIs = analogRead(forceAnalogInPin)-forceOffset;             // Força actual (força mesurada menys l'offset)
+ delay(10);                                                      // Retard de 10 milesimes de segon
+ positionDesired -= gain*(forceIs-forceDesired);                 // Posició desitjada: posició anterior - guany*(força actual - força desitjada)
+ myservo.write(positionDesired);                                 // Col·locar el servo a la posició nova
  }
